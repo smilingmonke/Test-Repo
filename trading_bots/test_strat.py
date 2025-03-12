2926f85effc706aebe1e2fa1258caa7ad7e4a92e
smilingmonke <bluemirage721@gmail.com>
2025-03-12 13:37:08 -0700

    adding a basic strat

---
+++ trading_bots/test_strat.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import numpy as np
+import pandas_ta as ta
+import plotly.graph_objects as go
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("trading_bots\\v75_D_1_2019-2025.csv")
+
+
+def support(df1, l, n1, n2):
+    for i in range(l - n1 + 1, l + 1):
+        if df1.low[i] > df1.low[i - 1]:
+            return 0
+
+    for i in range(l + 1, l + n2 + 1):
+        if df1.low[i] < df1.low[i - 1]:
+            return 0
+    return 1
+
+
+def resistance(df1, l, n1, n2):
+    for i in range(l - n1 + 1, l + 1):
+        if df1.high[i] < df1.high[i - 1]:
+            return 0
+
+    for i in range(l + 1, l + n2 + 1):
+        if df1.high[i] > df1.high[i - 1]:
+            return 0
+    return 1
+
+
+length = len(df)
+open = list(df["open"])
+high = list(df["high"])
+low = list(df["low"])
+close = list(df["close"])
+bodydiff = [0] * length
+highdiff = [0] * length
+lowdiff = [0] * length
+ratio1 = [0] * length
+ratio2 = [0] * length
+
+
+def isEngulfing(l):
+
+    row = l
+    bodydiff[row] = abs(open[row] - close[row])
+    if bodydiff[row] < 100:
+        bodydiff[row] = 100
+
+    bodydiffmin = 300
+    if (
+        bodydiff[row] > bodydiffmin
+        and bodydiff[row - 1] > bodydiffmin
+        and open[row - 1] < close[row - 1]
+        and open[row] > close[row]
+        and (open[row] - close[row]) >= -1e-2
+        and close[row] < open[row - 1]
+    ):
+        return 1
+    elif (
+        bodydiff[row] > bodydiffmin
+        and bodydiff[row - 1] > bodydiffmin
+        and open[row - 1] > close[row - 1]
+        and open[row] < close[row]
+        and (open[row] - close[row]) <= +1e-2
+        and close[row] > open[row - 1]
+    ):
+        return 2
+    else:
+        return 0
+
+
+def isStar(l):
+    bodydiffmin = 100
+    row = l
+    highdiff[row] = high[row] - max(open[row], close[row])
+    lowdiff[row] = min(open[row], close[row]) - low[row]
+    bodydiff[row] = abs(open[row] - close[row])
+    if bodydiff[row] < 100:
+        bodydiff[row] = 100
+    ratio1[row] = highdiff[row] / bodydiff[row]
+    ratio2[row] = lowdiff[row] / bodydiff[row]
+
+    if (
+        ratio1[row] > 1
+        and lowdiff[row] < 0.2 * highdiff[row]
+        and bodydiff[row] > bodydiffmin
+    ):
+        return 1
+    elif (
+        ratio2[row] > 1
+        and highdiff[row] < 0.2 * highdiff[row]
+        and bodydiff[row] > bodydiffmin
+    ):
+        return 2
+    else:
+        return 0
+
+
+def closeResistance(l, levels, lim):
+    if len(levels) == 0:
+        return 0
+
+    c1 = abs(df.high[l] - min(levels, key=lambda x: abs(x - df.high[l]))) <= lim
+    c2 = (
+        abs(
+            max(df.open[l], df.close[l])
+            - min(levels, key=lambda x: abs(x - df.high[l]))
+        )
+        <= lim
+    )
+    c3 = min(df.open[l], df.close[l]) < min(levels, key=lambda x: abs(x - df.high[l]))
+    c4 = df.low[l] < min(levels, key=lambda x: abs(x - df.high[l]))
+
+    if c1 or c2 and c3 and c4:
+        return 1
+    else:
+        return 0
+
+
+def closeSupport(l, levels, lim):
+    if len(levels) == 0:
+        return 0
+
+    c1 = abs(df.low[l] - min(levels, key=lambda x: abs(x - df.low[l]))) <= lim
+    c2 = (
+        abs(
+            max(df.open[l], df.close[l]) - min(levels, key=lambda x: abs(x - df.low[l]))
+        )
+        <= lim
+    )
+    c3 = min(df.open[l], df.close[l]) > min(levels, key=lambda x: abs(x - df.low[l]))
+    c4 = df.high[l] > min(levels, key=lambda x: abs(x - df.low[l]))
+
+    if c1 or c2 and c3 and c4:
+        return 1
+    else:
+        return 0
+
+
+n1 = 2
+n2 = 2
+backCandles = 45
+signal = [0] * length
+
+for row in range(backCandles, len(df) - n2):
+    ss = []
+    rr = []
+
+    for subrow in range(row - backCandles + n1, row + 1):
+        if support(df, subrow, n1, n2):
+            ss.append(df.low[subrow])
+        if resistance(df, subrow, n1, n2):
+            rr.append(df.high[subrow])
+
+    if (isEngulfing(row) == 1 or isStar(row) == 1) and closeResistance(row, rr, 3e3):
+        signal[row] = 1
+    elif (isEngulfing(row) == 2 or isStar(row) == 2) and closeSupport(row, ss, 3e3):
+        signal[row] = 2
+    else:
+        signal[row] = 0
+
+
+df["signal"] = signal
+
+print(df[df["signal"] == 1].count())
+
+
+SLTPRatio = 2
+
+
+def mytarget(barsupfront, df1):
+
+    length = len(df1)
+    high = list(df1["high"])
+    low = list(df1["low"])
+    open = list(df1["open"])
+    close = list(df1["close"])
+    signal = list(df1["signal"])
+    trendcat = [0] * length
+    amount = [0] * length
+
+    SL = 0
+    TP = 0
+    for line in range(backCandles, length - barsupfront - n2):
+
+        if signal[line] == 1:
+            SL = max(high[line - 1 : line + 1])
+            TP = close[line] - SLTPRatio * (SL - close[line])
+
+            for i in range(1, barsupfront + 1):
+                pass
