2ebfccec25f3e6ffc8b845b7017a873a35473283
smilingmonke <bluemirage721@gmail.com>
2025-04-16 22:52:09 -0700

    update

--- trading_bots/demo_bots/SMACross-10-20_15M(alert).py
+++ trading_bots/demo_bots/SMACross-10-20_15M(alert).py
@@ -31,15 +31,21 @@ def getData():
 
 def SMACross(x):
 
-    sma1 = x.MA10.iloc[-1]
-    sma2 = x.MA20.iloc[-1]
-    atr = x.ATR.iloc[-1]
-    atr_avg = x.ATR_Avg.iloc[-1]
+    sma1_prev = x.MA10.iloc[-3]
+    sma2_prev = x.MA20.iloc[-3]
+    sma1 = x.MA10.iloc[-2]
+    sma2 = x.MA20.iloc[-2]
+    atr = x.ATR.iloc[-2]
+    atr_avg = x.ATR_Avg.iloc[-2]
 
-    if sma1 > sma2 and atr > atr_avg:
+    if not sma1_prev > sma2_prev and sma1 > sma2 and atr > atr_avg:
         return 1
-    elif sma2 > sma1 and atr > atr_avg:
+    elif not sma2_prev > sma1_prev and sma2 > sma1 and atr > atr_avg:
         return -1
+    elif sma1 > sma2 and atr > atr_avg:
+        return 2
+    elif sma2 > sma1 and atr > atr_avg:
+        return -2
     else:
         return 0
 
@@ -55,20 +61,30 @@ def CrossAlert():
     if signal != 0:
 
         if signal == 1:
-            msg = f"Bullish movements on {symbol}"
+            msg = f"Bullish SMACROSS on {symbol}"
         elif signal == -1:
+            msg = f"Bearish SMACROSS on {symbol}"
+        elif signal == 2:
+            msg = f"Bullish movements on {symbol}"
+        elif signal == -2:
             msg = f"Bearish movements on {symbol}"
 
-        print(msg)
+        print(f"\n{msg}")
+        print("*" * 120)
+        print(df.tail(3))
+        print("*" * 120)
         uf.send_alert(msg)
+        print("Sleeping for 1/2 an hour😴")
+        time.sleep(1800)
 
     else:
+        print("*" * 120)
+        print(df.tail(3))
+        print("*" * 120)
         print("NO definite movemnt")
 
 
-CrossAlert()
-
-schedule.every(15).minutes.do(CrossAlert)
+schedule.every(1).minutes.do(CrossAlert)
 
 while True:
     try:
